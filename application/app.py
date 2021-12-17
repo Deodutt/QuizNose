2c384a7e7a66b54cbbe69ec2a0ed1506e49d0d37
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-12-17 00:57:04 -0500

    implemented a create table feature and list table feature

--- application/app.py
+++ application/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify, render_template
-from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+from sqlalchemy.sql.sqltypes import VARCHAR
 
 app = Flask(__name__)
+metadata = MetaData()
+# db = SQLAlchemy()
+
 
 # Connect to database
 engine = create_engine(
@@ -14,7 +19,46 @@ def list_tables():
     return list_of_tables
 
 
-print(list_tables())
+def create_table():
+    # checking the table list
+    list_of_tables = engine.table_names()
+    table_name = "user_info"
+
+    if table_name in list_of_tables:
+        print(f"The table '{table_name}' is inside the database!")
+
+    else:
+        user_info = Table(
+            table_name,
+            metadata,
+            Column("username", VARCHAR(20), primary_key=True),
+            Column("password", VARCHAR(20)),
+            Column("email", VARCHAR(20)),
+        )
+        metadata.create_all(engine)
+        print(f"The table '{table_name}' was successfully created!")
+    return
+
+
+# def delete_table():
+#     # checking the table list
+#     list_of_tables = engine.table_names()
+#     table_name = "users"
+
+#     if table_name in list_of_tables:
+#         print(f"The table '{table_name}'  was deleted from the database!")
+
+#     else:
+#         print(f"The table '{table_name}'  is not inside the database!")
+
+#     return
+
+
+print(f"Original List: {list_tables()}\n")
+create_table()
+print(f"New List: {list_tables()}\n")
+# delete_table()
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
