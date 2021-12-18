49762d7a9484c366734e99288ec8e1f976724197
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-12-18 15:15:30 -0500

    updated requirements.txt, gitignore, split db file

--- application/app.py
+++ application/app.py
@@ -1,22 +1,15 @@
 from flask import Flask, request, jsonify, render_template
-import MySQLdb
-# from functools import wraps
+import db
 
-# connection
-db = MySQLdb.connect(
-    host="database-1.cet4jo0trfys.us-east-1.rds.amazonaws.com",  # your host, usually localhost
-    user="admin",  # your username
-    passwd="KuraLabs#123",  # your password
-    db="final",
-)  # name of the data base
+# from functools import wraps
 
 ##This is a function for checking login. This function is invokved at every point of something requiring login
 
 
-#Barebones for API Calls
-#refs
-#https://pythonbasics.org/flask-http-methods/
-#https://stackoverflow.com/questions/40963401/flask-dynamic-data-update-without-reload-page/40964086
+# Barebones for API Calls
+# refs
+# https://pythonbasics.org/flask-http-methods/
+# https://stackoverflow.com/questions/40963401/flask-dynamic-data-update-without-reload-page/40964086
 
 # @app.route('/', methods = ["POST"])
 # def quiz_page():
@@ -25,72 +18,53 @@ db = MySQLdb.connect(
 # 			return redirect(url_for('results_page')
 
 # 	if request.method == "POST":
-# 		std_ans[request.form['$question num']] = request.form['$choice'].value 
+# 		std_ans[request.form['$question num']] = request.form['$choice'].value
 # 		return redirect(url_for('quiz_page',question=question+1))
 
-app = Flask(__name__, template_folder="templates", static_url_path='/static')
+app = Flask(__name__, template_folder="templates", static_url_path="/static")
 
-def is_logged(f):
-	@wraps(f)
-	def wrap(*args, **kwargs):
-		if 'logged_in' in session:
-			return f(*args, **kwargs)
-		else:
-			flash('Unauthorized, Please login','danger')
-			return redirect(url_for('login'))
-	return wrap
+# def is_logged(f):
+# 	@wraps(f)
+# 	def wrap(*args, **kwargs):
+# 		if 'logged_in' in session:
+# 			return f(*args, **kwargs)
+# 		else:
+# 			flash('Unauthorized, Please login','danger')
+# 			return redirect(url_for('login'))
+# 	return wrap
 
-@app.route('/')
+
+@app.route("/")
 def index():
-	return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/register')
+
+@app.route("/register")
 def register():
-	return render_template('register.html')
+    return render_template("register.html")
 
-@app.route('/studDash.html')
+
+@app.route("/studDash.html")
 def studDash():
-	return render_template('studDash.html')
+    return render_template("studDash.html")
 
-@app.route('/quiz')
+
+@app.route("/quiz")
 def quiz():
-	return render_template('quiz.html')
-
-
-def list_tables(db_table):
-    cur = db.cursor()
-    cur.execute(f"SHOW TABLES FROM {db_table};")
-    result = cur.fetchall()
-    return result
-
-
-def create_table(new_name):
-    cur = db.cursor()
-    cur.execute(
-        f"CREATE TABLE IF NOT EXISTS {new_name} (quiz_id VARCHAR(20) NOT NULL, question_number INT NOT NULL, choice_a TEXT NOT NULL, choice_b TEXT NOT NULL, choice_c TEXT NOT NULL, choice_d TEXT NOT NULL);"
-    )
-
-    result = cur.fetchall()
-    return print(f"The table '{new_name}' was successfully created!")
-
-
-def delete_table(target):
-    cur = db.cursor()
-    cur.execute(f"DROP TABLE {target};")
-    result = cur.fetchall()
-    return print(f"The table '{target}' was successfully deleted!")
+    return render_template("quiz.html")
 
 
 # This list all the tables inside the database final
-print(f"Original List: {list_tables(db_table = 'final')}\n")
+print(f"Original List: {db.list_tables(db_table = 'final')}\n")
 
 # This creates a table named <var>
-create_table(new_name="testing_purpose")
-print(f"New List: {list_tables(db_table = 'final')}\n")
+db.create_table(new_name="testing_purpose")
+print(f"New List: {db.list_tables(db_table = 'final')}\n")
 
 # This deletes a table named <var>
-delete_table(target="testing_purpose")
-print(f"New List: {list_tables(db_table = 'final')}\n")
+db.delete_table(target="testing_purpose")
+print(f"New List: {db.list_tables(db_table = 'final')}\n")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
---
+++ application/db.py
@@ -0,0 +1,32 @@
+import MySQLdb
+
+# connection
+db = MySQLdb.connect(
+    host="database-1.cet4jo0trfys.us-east-1.rds.amazonaws.com",  # your host, usually localhost
+    user="admin",  # your username
+    passwd="KuraLabs#123",  # your password
+    db="final",
+)  # name of the data base
+
+
+def list_tables(db_table):
+    cur = db.cursor()
+    cur.execute(f"SHOW TABLES FROM {db_table};")
+    result = cur.fetchall()
+    return result
+
+
+def create_table(new_name):
+    cur = db.cursor()
+    cur.execute(
+        f"CREATE TABLE IF NOT EXISTS {new_name} (quiz_id VARCHAR(20) NOT NULL, question_number INT NOT NULL, choice_a TEXT NOT NULL, choice_b TEXT NOT NULL, choice_c TEXT NOT NULL, choice_d TEXT NOT NULL);"
+    )
+    result = cur.fetchall()
+    return print(f"The table '{new_name}' was successfully created!")
+
+
+def delete_table(target):
+    cur = db.cursor()
+    cur.execute(f"DROP TABLE {target};")
+    result = cur.fetchall()
+    return print(f"The table '{target}' was successfully deleted!")
