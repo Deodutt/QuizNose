5aa0150f721c1b5eab8bd5e129fea66604944bc4
kawang.wong <nycazn724@gmail.com>
2021-12-18 15:41:57 -0500

    change

---
+++ application/classes.py
@@ -0,0 +1,10 @@
+class RegisterForm(Form):
+	name = StringField('Name', [validators.Length(min=3, max=50)])
+	username = StringField('Username', [validators.Length(min=4,max=25)])
+	email = StringField('Email', [validators.Email()])
+	password = PasswordField('Password', [
+			validators.Regexp("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", message="Password should contain min 8 characters including 1 letter and 1 number."),
+			validators.DataRequired(),
+			validators.EqualTo('confirm', message="Password do not match")
+		])
+	confirm = PasswordField('Confirm Password')
