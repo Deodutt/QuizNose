8e70a6465a7968b3696a2daff70e7e1d9677a8bc
kawang.wong <nycazn724@gmail.com>
2021-12-27 17:43:53 -0500

    updated registration

--- application/classes.py
+++ application/classes.py
@@ -1,12 +1,18 @@
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators, DateTimeField, BooleanField, IntegerField
 
 class RegisterForm(Form):
-	name = StringField('Name', [validators.Length(min=3, max=50)])
-	username = StringField('Username', [validators.Length(min=4,max=25)])
-	email = StringField('Email', [validators.Email()])
+	name = StringField('Name', [validators.Length(min=3, max=30)])
+	username = StringField('Username', [validators.Length(min=4,max=30)])
+	email = StringField('Email', [
+		validators.Email(),
+		validators.Length(min=5, max=30)
+		])
 	password = PasswordField('Password', [
 			validators.Regexp("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", message="Password should contain min 8 characters including 1 letter and 1 number."),
 			validators.DataRequired(),
-			validators.EqualTo('confirm', message="Password do not match")
+			validators.EqualTo('confirm', message="Password do not match"),
+			validators.Length(min=8, max=20)
 		])
 	confirm = PasswordField('Confirm Password')
+
+	##this class is necessary to create parameters for what the registration parameters are
