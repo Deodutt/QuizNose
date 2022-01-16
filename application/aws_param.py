dd2bace5674cc67a5421b2b4480bc805f4a6f3bb
KennethT404 <tankenneth98@gmail.com>
2022-01-16 18:33:59 -0500

    minor changes to get it to run locally

--- application/aws_param.py
+++ application/aws_param.py
@@ -1,10 +1,10 @@
 import boto3
 
-def get_ssm_param(param_name: str):
+def get_ssm_parameter(param_name: str):
     """Returns SSM value given a parameter name"""
     ssm = boto3.client("ssm")  # selecting what service
 
     parameter = ssm.get_parameter(
         Name=param_name, WithDecryption=True
     )  # grabbing the paramaeter that has the name/prod/db/password
-    return parameter["Value"]
+    return parameter["Parameter"]["Value"]
