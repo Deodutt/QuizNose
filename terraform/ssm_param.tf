f602bf27292f9bbb48a9a35f605694059111c56d
KennethT404 <tankenneth98@gmail.com>
2022-01-17 20:43:09 -0500

    added in ssm changes for email user and pass

--- terraform/ssm_param.tf
+++ terraform/ssm_param.tf
@@ -2,24 +2,24 @@ resource "aws_ssm_parameter" "db_pass" {
   name  = "/QUIZNOSE/DB_PASS"
   type  = "SecureString"
   value = "KuraLabs#123"
-  # value = ${{DB_PASS}}
 }
 
 resource "aws_ssm_parameter" "api_key" {
   name  = "/QUIZNOSE/API_KEY"
   type  = "SecureString"
   value = "at_zmz5m4BQ9kegkMqfGaTujPCCUD135"
-  # value = ${{API_KEY}}
 }
 
-# resource "aws_ssm_parameter" "email_pass" {
-#   name  = "/QUIZNOSE/EMAIL_PASS"
-#   type  = "SecureString"
-#   # value = ${{EMAIL_PASS}}
-# }
+resource "aws_ssm_parameter" "email_user" {
+  name  = "/QUIZNOSE/EMAIL_USER"
+  type  = "SecureString"
+  value = "Kuralabs123"
+}
 
-# resource "aws_ssm_parameter" "email_user" {
-#   name  = "/QUIZNOSE/ENAIL_USER"
-#   type  = "SecureString"
-#   # value = ${{EMAIL_USER}}
-# }
+resource "aws_ssm_parameter" "email_pass" {
+  name  = "/QUIZNOSE/EMAIL_PASS"
+  type  = "SecureString"
+  value = "quiznose.notification@gmail.com"
+}
+
+
